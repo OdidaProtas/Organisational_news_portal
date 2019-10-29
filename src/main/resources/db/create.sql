4f357c2ec22c13c60f98947878e23f544687adf1
Brian Odida <bryodiiidah@gmail.com>
2019-10-29 09:05:34 +0300

    adds file to create h2 databases

---
+++ src/main/resources/db/create.sql
@@ -0,0 +1,32 @@
+SET MODE PostgreSQL;
+
+CREATE TABLE IF NOT EXISTS departments(
+    id int PRIMARY KEY auto_increment,
+    name VARCHAR,
+    description VARCHAR,
+    numberOfEmployees int
+);
+
+CREATE TABLE IF NOT EXISTS news(
+    id int PRIMARY KEY auto_increment,
+    title VARCHAR,
+    newsContent VARCHAR,
+    writtenBy VARCHAR,
+    departmentId int,
+--    categoryId int
+);
+
+CREATE TABLE IF NOT EXISTS departments_users(
+        id int PRIMARY KEY auto_increment,
+        departmentId int,
+        usersId int
+);
+
+CREATE TABLE IF NOT EXISTS users(
+    id int PRIMARY KEY auto_increment,
+    title VARCHAR,
+    name VARCHAR,
+    position VARCHAR,
+    role VARCHAR,
+    departmentId int
+);
